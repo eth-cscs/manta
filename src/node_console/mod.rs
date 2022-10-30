cdde5e49d131bb8ab5d043fa080b9e67fb353cec
Manuel Sopena Ballesteros <msopena@cscs.ch>
2022-10-30 03:09:02 +0100

    add new feature to open an interactive tty connection to a container and have a shell open on the other end working with local terminal

--- src/node_console/mod.rs
+++ src/node_console/mod.rs
@@ -1,9 +1,10 @@
-use std::{error::Error, io::Write};
+use std::{error::Error, io::{Write, Read, stdout}};
 
 use futures_util::StreamExt;
 use k8s_openapi::api::core::v1::Pod;
 use kube::{Api, api::{AttachParams, AttachedProcess}};
 use serde_json::Value;
+use termion::{raw::IntoRawMode, color};
 use tokio::{io::{AsyncWriteExt}, runtime::Runtime};
 
 use crate::shasta_cfs_session_logs::client::get_k8s_client_programmatically;
@@ -21,23 +22,30 @@ pub async fn connect_to_console(xname: &str) -> Result<(), Box<dyn Error>> {
     let console_operator_pod = &pods.items[0];
     let console_operator_pod_name = console_operator_pod.metadata.name.clone().unwrap();
     
-    let attached = pods_api.exec(
+    let mut attached = pods_api.exec(
         &console_operator_pod_name, 
         vec!["sh", "-c", &format!("/app/get-node {}", xname)], 
         &AttachParams::default().container("cray-console-operator").stderr(false)
     ).await?;
 
-    let output = get_exec_command_output(attached).await;
-    let output_json: Value = serde_json::from_str(&output)?;
+    // let output = get_exec_command_output(attached).await;
+    let mut stdout_stream = tokio_util::io::ReaderStream::new(attached.stdout().unwrap());
+    let next_stdout = stdout_stream.next().await;
+    let stdout_str = String::from_utf8(next_stdout.unwrap().unwrap().to_vec()).unwrap();
+    let output_json: Value = serde_json::from_str(&stdout_str)?;
 
     let console_pod_name = output_json["podname"].as_str().unwrap();
 
-    log::info!("Connecting to console {} - kubectl -n services exec -it {} -c cray-console-node -- sh", xname, console_pod_name);
+    // let command = vec!["conman", "-j", xname];
+    let command = vec!["bash"];
+
+    log::info!("Alternatively run - kubectl -n services exec -it {} -c cray-console-node -- {}", console_pod_name, command.iter().map(|x| x.to_string() + " ").collect::<String>());
+
+    log::info!("Connecting to console {}", xname);
 
     let mut attached = pods_api.exec(
         &console_pod_name, 
-        vec!["conman", "-j", xname], 
-        // vec!["sh"],
+        command,
         &AttachParams::default()
             .container("cray-console-node")
             .stdin(true)
@@ -45,66 +53,77 @@ pub async fn connect_to_console(xname: &str) -> Result<(), Box<dyn Error>> {
             .stderr(false) // Note to self: tty and stderr cannot both be true
             .tty(true)
     ).await?;
-
-    let mut user_input;
-    let stdin = std::io::stdin(); // We get `Stdin` here.
     
+    println!("Connected to {}{}{}!", color::Fg(color::Blue), xname, color::Fg(color::Reset));
+    println!("Use {}&.{} key combination to exit the console.", color::Fg(color::Green), color::Fg(color::Reset));
+
+
     let mut stdin_writer = attached.stdin().unwrap();
     let mut stdout_stream = tokio_util::io::ReaderStream::new(attached.stdout().unwrap());
 
-    let mut next_stdout;
-    let mut stdout;
+    let mut stdin = std::io::stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
 
-    user_input = String::new();
-    
     let rt = Runtime::new().unwrap();
     rt.spawn(async move {
+
+        let mut next_stdout;
+        let mut stdout_str;
+
         loop {
-            // println!("Waiting user input");
 
-            stdin.read_line(&mut user_input).unwrap();
-
-            // println!("Writting {} in string and {:?} in bytes", user_input.as_str(), user_input.as_bytes());
-
-            stdin_writer.write_all(format!("{}", user_input).as_bytes()).await.unwrap();
+            next_stdout = stdout_stream.next().await;
+            stdout_str = String::from_utf8(next_stdout.unwrap().unwrap().to_vec()).unwrap();
+            print!("{}", stdout_str);
+            stdout.flush().unwrap();
         }
-    }); 
+    });
 
     loop {
-        // Read command output
-        // println!("START READING CMD OUTPUT??");
-        next_stdout = stdout_stream.next().await;
-        stdout = String::from_utf8(next_stdout.unwrap()?.to_vec()).unwrap();
-        print!("{}", stdout);
-        std::io::stdout().flush().unwrap();
-        // println!("END READING CMD OUTPUT??");
+        let mut buffer = [0; 1];
+
+        // read up to 10 bytes
+        let n = stdin.read(&mut buffer[..])?;
+
+        stdin_writer.write_all(format!("{}", String::from_utf8((&buffer[..n]).to_vec()).unwrap()).as_bytes()).await?;
     }
- 
-        // // Read echo
-        // if (!user_input.is_empty()) {
-        //     println!("START READING ECHO??");
-        //     next_stdout = stdout_stream.next().await;
-        //     stdout = String::from_utf8(next_stdout.unwrap()?.to_vec()).unwrap();
-        //     // print!("{}", stdout);
-        //     // std::io::stdout().flush().unwrap();
-        //     println!("END READING ECHO??");
-        // }
 
-        // // Read command output
-        // println!("START READING CMD OUTPUT??");
-        // next_stdout = stdout_stream.next().await;
-        // stdout = String::from_utf8(next_stdout.unwrap()?.to_vec()).unwrap();
-        // print!("{}", stdout);
-        // std::io::stdout().flush().unwrap();
-        // println!("END READING CMD OUTPUT??");
+    // let mut stdin_writer = attached.stdin().unwrap();
+    // let mut stdout_stream = tokio_util::io::ReaderStream::new(attached.stdout().unwrap());
+    
+    // let stdin = std::io::stdin(); // We get `Stdin` here.
+    // let rt = Runtime::new().unwrap();
+    // rt.spawn(async move {
+    //     let mut user_input = String::new();
+    //     loop {
+    //         stdin.read_line(&mut user_input).unwrap();
+    //         stdin_writer.write_all(user_input.as_bytes()).await.unwrap();
+    //     }
+    // });
 
-        // // Read shell prompt
-        // println!("START READING SHELL PROMPT??");
-        // next_stdout = stdout_stream.next().await;
-        // stdout = String::from_utf8(next_stdout.unwrap()?.to_vec()).unwrap();
-        // print!("{}", stdout);
-        // std::io::stdout().flush().unwrap();
-        // println!("END READING SHELL PROMPT??");
+    // // let mut stdin = termion::async_stdin().keys();
+    // // let rt = Runtime::new().unwrap();
+    // // rt.spawn(async move {
+    // //     loop {
+    // //         let user_input = stdin.next();
+    // //         if user_input.is_some() {
+    // //             println!("Hi");
+    // //             let user_input_b = user_input.unwrap().unwrap();
+    // //             println!("{:?}", user_input_b);
+    // //             stdin_writer.write_all(&vec![user_input_b, String::from("\n").as_bytes()[0]]).await.unwrap();
+    // //         }
+    // //     }
+    // // });
+
+    // let mut next_stdout;
+    // let mut stdout_str;
+
+    // loop {
+    //     // Read command output
+    //     next_stdout = stdout_stream.next().await;
+    //     stdout_str = String::from_utf8(next_stdout.unwrap()?.to_vec()).unwrap();
+    //     print!("{}", stdout_str);
+    //     std::io::stdout().flush().unwrap();
     // }
 
 }
