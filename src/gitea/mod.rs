74a33b887d801eb3e7f75c21e9d0071a4530472a
Manuel Sopena Ballesteros <msopena@cscs.ch>
2023-01-10 14:34:26 +0100

    fix bug with manta not fetching the right gitea url to fetch commit details

--- src/gitea/mod.rs
+++ src/gitea/mod.rs
@@ -5,13 +5,17 @@ pub mod http_client {
 
     pub async fn get_commit_details(repo_url: &str, commitid: &str, gitea_token: &str) -> core::result::Result<Value, Box<dyn std::error::Error>> {
 
-        let settings = config::get("config");
-        let gitea_base_url = settings.get::<String>("gitea_base_url").unwrap();
-        let gitea_api_base_url = format!("{}{}", gitea_base_url, "/api/v1");
+        // let settings = config::get("config");
+        let gitea_internal_base_url = "https://api-gw-service-nmn.local/vcs";
+        let gitea_api_base_url = format!("{}{}", gitea_internal_base_url, "/api/v1");
 
         log::debug!("Repo URL: {}", repo_url);
 
-        let repo_name = repo_url.trim_start_matches(&gitea_base_url).trim_end_matches(".git");
+        let repo_name = repo_url.trim_start_matches(&gitea_internal_base_url).trim_end_matches(".git");
+
+        log::info!("repo_url: {}", repo_url);
+        log::info!("gitea_base_url: {}", gitea_internal_base_url);
+        log::info!("repo_name: {}", repo_name);
 
         let client;
 
