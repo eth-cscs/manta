236c4bd618be777e208be884004a82a7a6429d55
Manuel Sopena Ballesteros <msopena@cscs.ch>
2023-08-22 10:59:59 +0200

    refactor code and add validation when trying to access interactive session to a target node of a cfs session to build an image

--- src/common.rs
+++ src/common.rs
@@ -1,10 +1,11 @@
+pub mod bos_sessiontemplate_utils;
+pub mod cfs_session_utils;
 pub mod cluster_ops;
-pub mod ims_ops;
 pub mod gitea;
+pub mod ims_ops;
 pub mod jwt_ops;
 pub mod local_git_repo;
 pub mod log_ops;
 pub mod node_ops;
+pub mod terminal_ops;
 pub mod vault;
-pub mod cfs_session_utils;
-pub mod bos_sessiontemplate_utils;
