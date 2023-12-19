c538ba763f19be4c051c53239a8aa5fcc255f4c0
Manuel Sopena Ballesteros <manuel.sopena@cscs.ch>
2023-12-19 12:56:02 +0100

    refactor: rename method name to get multiple CFS components

--- src/cli/commands/get_nodes.rs
+++ src/cli/commands/get_nodes.rs
@@ -24,7 +24,7 @@ pub async fn exec(
 
     hsm_groups_node_list.sort();
 
-    let node_details_list = mesa::cfs::component::mesa::http_client::exec(
+    let node_details_list = mesa::cfs::component::mesa::http_client::get_members(
         shasta_token,
         shasta_base_url,
         shasta_root_cert,
