58ab2d5ae455a3c4ee919a77cb595a2529f4fd88
Manuel Sopena Ballesteros <manuel.sopena@cscs.ch>
2024-05-23 22:38:31 +0200

    feat: new command 'get cluster <cluster name> --output summary'

--- src/cli/commands/get_nodes.rs
+++ src/cli/commands/get_nodes.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use comfy_table::{Cell, Table};
 use mesa::hsm;
 
 use crate::common::node_ops;
@@ -93,6 +96,94 @@ pub async fn exec(
             "{}",
             serde_json::to_string_pretty(&node_details_list).unwrap()
         );
+    } else if output_opt.is_some() && output_opt.unwrap().eq("summary") {
+        let mut power_status_counters: HashMap<String, usize> = HashMap::new();
+        let mut boot_configuration_counters: HashMap<String, usize> = HashMap::new();
+        let mut runtime_configuration_counters: HashMap<String, usize> = HashMap::new();
+        let mut boot_image_counters: HashMap<String, usize> = HashMap::new();
+
+        for node in node_details_list {
+            power_status_counters
+                .entry(node.power_status)
+                .and_modify(|power_status_counter| *power_status_counter += 1)
+                .or_insert(1);
+
+            boot_configuration_counters
+                .entry(node.boot_configuration)
+                .and_modify(|power_status_counter| *power_status_counter += 1)
+                .or_insert(1);
+
+            runtime_configuration_counters
+                .entry(node.desired_configuration)
+                .and_modify(|power_status_counter| *power_status_counter += 1)
+                .or_insert(1);
+
+            boot_image_counters
+                .entry(node.boot_image_id)
+                .and_modify(|power_status_counter| *power_status_counter += 1)
+                .or_insert(1);
+        }
+
+        let mut table = Table::new();
+
+        table.set_header(vec!["Power status", "Num nodes"]);
+
+        for power_status in ["FAILED", "ON", "OFF", "READY", "STANDBY", "UNCONFIGURED"] {
+            table
+                .load_preset(comfy_table::presets::ASCII_FULL_CONDENSED)
+                .add_row(vec![
+                    Cell::new(power_status),
+                    Cell::new(power_status_counters.get(power_status).unwrap_or(&0))
+                        .set_alignment(comfy_table::CellAlignment::Center),
+                ]);
+        }
+
+        println!("{table}");
+
+        let mut table = Table::new();
+
+        table.set_header(vec!["Boot configuration name", "Num nodes"]);
+
+        for (config_name, counter) in boot_configuration_counters {
+            table
+                .load_preset(comfy_table::presets::ASCII_FULL_CONDENSED)
+                .add_row(vec![
+                    Cell::new(config_name),
+                    Cell::new(counter).set_alignment(comfy_table::CellAlignment::Center),
+                ]);
+        }
+
+        println!("{table}");
+
+        let mut table = Table::new();
+
+        table.set_header(vec!["Boot image id", "Num nodes"]);
+
+        for (image_id, counter) in boot_image_counters {
+            table
+                .load_preset(comfy_table::presets::ASCII_FULL_CONDENSED)
+                .add_row(vec![
+                    Cell::new(image_id),
+                    Cell::new(counter).set_alignment(comfy_table::CellAlignment::Center),
+                ]);
+        }
+
+        println!("{table}");
+
+        let mut table = Table::new();
+
+        table.set_header(vec!["Runtime configuration name", "Num nodes"]);
+
+        for (config_name, counter) in runtime_configuration_counters {
+            table
+                .load_preset(comfy_table::presets::ASCII_FULL_CONDENSED)
+                .add_row(vec![
+                    Cell::new(config_name),
+                    Cell::new(counter).set_alignment(comfy_table::CellAlignment::Center),
+                ]);
+        }
+
+        println!("{table}");
     } else {
         node_ops::print_table(node_details_list);
     }
