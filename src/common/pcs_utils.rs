f8b848bea494d7f7c9dbbf7ca9ec219b59a357bd
Manuel Sopena Ballesteros <manuel.sopena@cscs.ch>
2024-08-23 23:09:10 +0200

    feat: add pcs utils

---
+++ src/common/pcs_utils.rs
@@ -0,0 +1,31 @@
+use comfy_table::Table;
+use serde_json::Value;
+
+pub fn print_summary_table(transition: Value) {
+    let tasks: Vec<Value> = transition["tasks"].as_array().unwrap().to_vec();
+
+    let mut table = Table::new();
+
+    println!(
+        "\nTransition ID: {}",
+        transition["transitionID"].as_str().unwrap()
+    );
+    println!(
+        "Transition Status: {}",
+        transition["transitionStatus"].as_str().unwrap()
+    );
+
+    table
+        .load_preset(comfy_table::presets::ASCII_FULL_CONDENSED)
+        .set_header(vec!["XNAME", "Status", "Description"]);
+
+    for task in tasks {
+        table.add_row(vec![
+            task["xname"].as_str().unwrap(),
+            task["taskStatus"].as_str().unwrap(),
+            task["taskStatusDescription"].as_str().unwrap(),
+        ]);
+    }
+
+    println!("{table}");
+}
