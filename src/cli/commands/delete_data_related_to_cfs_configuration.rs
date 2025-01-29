bf84d3466cb691d4c78ed43853602a78d16ababf
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-01-29 09:52:12 +0100

    refactor: group trait methods

--- src/cli/commands/delete_data_related_to_cfs_configuration.rs
+++ src/cli/commands/delete_data_related_to_cfs_configuration.rs
@@ -3,7 +3,6 @@ use std::collections::HashMap;
 
 use std::io::{self, Write};
 
-use backend_dispatcher::contracts::BackendTrait;
 use backend_dispatcher::interfaces::group::GroupTrait;
 use chrono::NaiveDateTime;
 use comfy_table::Table;
--- src/cli/commands/power_off_cluster.rs
+++ src/cli/commands/power_off_cluster.rs
@@ -1,4 +1,4 @@
-use backend_dispatcher::{contracts::BackendTrait, interfaces::group::GroupTrait};
+use backend_dispatcher::interfaces::group::GroupTrait;
 use dialoguer::{theme::ColorfulTheme, Confirm};
 use mesa::{error::Error, pcs};
 
