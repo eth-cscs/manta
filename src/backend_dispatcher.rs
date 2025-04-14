788e7e7927e7b1d1bd061b649ede92d9b694b262
Manuel Sopena Ballesteros <masber@hotmail.com>
2025-04-14 17:45:54 +0200

    feat: remove 'mesa' dependencies in subcommand 'migrate restore'

--- src/backend_dispatcher.rs
+++ src/backend_dispatcher.rs
@@ -15,7 +15,7 @@ use backend_dispatcher::{
     interfaces::{
         apply_hw_cluster_pin::ApplyHwClusterPin,
         apply_session::ApplySessionTrait,
-        bos::ClusterSessionTrait,
+        bos::{ClusterSessionTrait, ClusterTemplateTrait},
         bss::BootParametersTrait,
         cfs::CfsTrait,
         get_bos_session_templates::GetTemplatesTrait,
@@ -1499,6 +1499,116 @@ impl ClusterSessionTrait for StaticBackendDispatcher {
                     shasta_base_url,
                     shasta_root_cert,
                     bos_session,
+                )
+                .await
+            }
+        }
+    }
+}
+
+impl ClusterTemplateTrait for StaticBackendDispatcher {
+    async fn get_template(
+        &self,
+        shasta_token: &str,
+        shasta_base_url: &str,
+        shasta_root_cert: &[u8],
+        bos_session_template_id_opt: Option<&str>,
+    ) -> Result<Vec<BosSessionTemplate>, Error> {
+        match self {
+            CSM(b) => {
+                b.get_template(
+                    shasta_token,
+                    shasta_base_url,
+                    shasta_root_cert,
+                    bos_session_template_id_opt,
+                )
+                .await
+            }
+            OCHAMI(b) => {
+                b.get_template(
+                    shasta_token,
+                    shasta_base_url,
+                    shasta_root_cert,
+                    bos_session_template_id_opt,
+                )
+                .await
+            }
+        }
+    }
+
+    async fn get_all_templates(
+        &self,
+        shasta_token: &str,
+        shasta_base_url: &str,
+        shasta_root_cert: &[u8],
+    ) -> Result<Vec<BosSessionTemplate>, Error> {
+        match self {
+            CSM(b) => {
+                b.get_all_templates(shasta_token, shasta_base_url, shasta_root_cert)
+                    .await
+            }
+            OCHAMI(b) => {
+                b.get_all_templates(shasta_token, shasta_base_url, shasta_root_cert)
+                    .await
+            }
+        }
+    }
+
+    async fn put_template(
+        &self,
+        shasta_token: &str,
+        shasta_base_url: &str,
+        shasta_root_cert: &[u8],
+        bos_template: &BosSessionTemplate,
+        bos_template_name: &str,
+    ) -> Result<BosSessionTemplate, Error> {
+        match self {
+            CSM(b) => {
+                b.put_template(
+                    shasta_token,
+                    shasta_base_url,
+                    shasta_root_cert,
+                    bos_template,
+                    bos_template_name,
+                )
+                .await
+            }
+            OCHAMI(b) => {
+                b.put_template(
+                    shasta_token,
+                    shasta_base_url,
+                    shasta_root_cert,
+                    bos_template,
+                    bos_template_name,
+                )
+                .await
+            }
+        }
+    }
+
+    async fn delete_template(
+        &self,
+        shasta_token: &str,
+        shasta_base_url: &str,
+        shasta_root_cert: &[u8],
+        bos_template_id: &str,
+    ) -> Result<(), Error> {
+        match self {
+            CSM(b) => {
+                b.delete_template(
+                    shasta_token,
+                    shasta_base_url,
+                    shasta_root_cert,
+                    bos_template_id,
+                )
+                .await
+            }
+            OCHAMI(b) => {
+                b.delete_template(
+                    shasta_token,
+                    shasta_base_url,
+                    shasta_root_cert,
+                    bos_template_id,
                 )
                 .await
             }
